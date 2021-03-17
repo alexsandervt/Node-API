093b3d5ed61648a0d2051371ff4ef3623df450e5
Alexsander Telles <git@alextelles.dev.br>
2021-03-16 21:31:32 -0300

    API / Validação de login

---
+++ src/app/controllers/LoginController.js
@@ -0,0 +1,35 @@
+import bcrypt from "bcryptjs";
+import User from "../models/User";
+
+class LoginController {
+  async store(req, res) {
+    const { email, password } = req.body;
+
+    const userExiste = await User.findOne({ email: email });
+
+    if (!userExiste) {
+      return res.status(401).json({
+        error: true,
+        code: 110,
+        message: "Usuário não encontrado",
+      });
+    }
+
+    if (!(await bcrypt.compare(password, userExiste.password))) {
+      return res.status(401).json({
+        error: true,
+        code: 111,
+        message: "Senha inválida",
+      });
+    }
+    return res.json({
+      user: {
+        id: userExiste._id,
+        name: userExiste.name,
+        email,
+      },
+    });
+  }
+}
+
+export default new LoginController();
--- src/routes.js
+++ src/routes.js
@@ -2,27 +2,11 @@ import { Router } from "express";
 import mongoose from "mongoose";
 import User from "./app/models/User";
 import UserControler from "./app/controllers/UserController";
+import LoginController from "./app/controllers/LoginController";
 
 const routes = new Router();
 
 routes.post("/users", UserControler.store);
-
-routes.get("/", async (req, res) => {
-  // await User.create(
-  //   {
-  //     name: "Telles6",
-  //     email: "alex@telles.dev.br",
-  //     password: "123456",
-  //   },
-  //   function (err, small) {
-  //     if (err)
-  //       return res.status(400).json({ error: "Erro: Ao cadastrar usuário!" });
-  //     return res.status(200).json({ error: "usuario cadastrado com sucesso!" });
-  //   }
-  // );
-});
-routes.get("/contato", (req, res) => {
-  res.send("Contato");
-});
+routes.post("/login", LoginController.store);
 
 export default routes;
